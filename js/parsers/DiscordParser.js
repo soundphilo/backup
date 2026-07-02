0954c84226b208a489d57b9adfec3c408711fecc
soundphilo <matsunokesannan3@gmail.com>
2026-07-03 08:14:00 +0900

    Update DiscordParser.js

--- js/parsers/DiscordParser.js
+++ js/parsers/DiscordParser.js
@@ -72,7 +72,9 @@ class DiscordParser {
       // 타임스탬프 라인 = 새 메시지 시작
       const tsMatch = line.match(this._regexTimestamp);
       if (tsMatch) {
-        if (currentMessage) messages.push(currentMessage);
+        if (currentMessage) {
+          currentMessage.chatMessage = currentMessage.chatMessage.trimEnd();
+          messages.push(currentMessage);
         currentMessage = {
           time: tsMatch[1].trim(),
           username: tsMatch[2].trim(),
